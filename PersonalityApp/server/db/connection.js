ae9386eb0af556eb7c9264541cc06caa1140e06d
Ben Andrew <benyaleandrew@gmail.com>
2026-03-27 12:12:27 -0400

    fixed connection.js, server.js, env.js

--- PersonalityApp/server/db/connection.js
+++ PersonalityApp/server/db/connection.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
- 
-const uri = process.env.ATLAS_URI || "";
- 
-try {
-  await mongoose.connect(uri, {
-    dbName: "personality_app",
-  });
-  console.log("Successfully connected to MongoDB via Mongoose!");
-} catch (err) {
-  console.error("MongoDB connection error:", err);
-  process.exit(1); // Exit if the database connection fails
-}
- 
-export default mongoose.connection;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI, {
+      dbName: "personality_app",
+    });
+    console.log("Successfully connected to MongoDB via Mongoose!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- PersonalityApp/server/server.js
+++ PersonalityApp/server/server.js
@@ -1,18 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./config.env" });
+import "./config/env.js";  // MUST be first import
 import express from "express";
 import cors from "cors";
-import "./db/connection.js";
+import connectDB from "./db/connection.js";
 import login from "./routes/login.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 app.use("/login", login);
 
-// start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
