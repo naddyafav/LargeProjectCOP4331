1a5861e913fc035a03f7180e6ab490745fd990c0
Ben Andrew <benyaleandrew@gmail.com>
2026-03-27 15:42:00 -0400

    commented out auth on server.js for test

--- PersonalityApp/server/server.js
+++ PersonalityApp/server/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db/connection.js";
 import login from "./routes/login.js";
-import auth from "./routes/auth.js";
+//import auth from "./routes/auth.js";
 
 
 const PORT = process.env.PORT || 5050;
@@ -14,8 +14,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/login", login);
-app.use("/auth", auth);
-app.use("/auth", auth);  
+//app.use("/auth", auth);
+//app.use("/auth", auth);  
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
