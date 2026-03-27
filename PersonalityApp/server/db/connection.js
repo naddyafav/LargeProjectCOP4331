8cf62b7c68e8e34ab26b9c52eb8492cd33712c03
Ben Andrew <benyaleandrew@gmail.com>
2026-03-26 20:28:29 -0400

    updated connection.js for mongoose and db name

--- PersonalityApp/server/db/connection.js
+++ PersonalityApp/server/db/connection.js
@@ -1,26 +1,15 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-
+import mongoose from "mongoose";
+ 
 const uri = process.env.ATLAS_URI || "";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
+ 
 try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log(
-   "Pinged your deployment. You successfully connected to MongoDB!"
-  );
-} catch(err) {
-  console.error(err);
+  await mongoose.connect(uri, {
+    dbName: "personality_app",
+  });
+  console.log("Successfully connected to MongoDB via Mongoose!");
+} catch (err) {
+  console.error("MongoDB connection error:", err);
+  process.exit(1); // Exit if the database connection fails
 }
-
-let db = client.db("employees");
-
-export default db;
+ 
+export default mongoose.connection;
