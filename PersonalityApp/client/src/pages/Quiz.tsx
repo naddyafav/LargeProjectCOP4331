77027dfea2cff0c260b1b756ca4f16916f0a594b
Ben Andrew <benyaleandrew@gmail.com>
2026-04-10 17:31:43 -0400

    added basic buttons to quiz and friends pages for navigation

--- PersonalityApp/client/src/pages/Quiz.tsx
+++ PersonalityApp/client/src/pages/Quiz.tsx
@@ -1,5 +1,59 @@
-//import React from "react";
+//import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Clouds from "../components/Clouds";//import React from "react";
 
 export default function Quiz() {
-  return <div>Quiz Page</div>;
+
+  const navigate = useNavigate();
+  const goToHome = () => { navigate("/home"); };
+
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ width: "100%", height: "100vh", backgroundColor: "#ACDFFA", position: "relative", overflow: "hidden" }}
+    >
+      
+      <Clouds />
+
+      {/* Card */}
+      <div
+        className="p-4"
+        style={{
+          width: "100%",
+          maxWidth: "420px",
+          backgroundColor: "#f8f9fa",
+          borderRadius: "20px",
+          border: "6px solid #7aa2e3",
+          boxShadow: "0 4px 15px rgba(0,0,0,0.1)",
+          zIndex: 1,
+        }}
+      >
+        <div className="text-center mb-4">
+          <div
+            style={{
+              width: "70px",
+              height: "70px",
+              backgroundColor: "#7aa2e3",
+              borderRadius: "50%",
+              margin: "0 auto",
+            }}
+          ></div>
+          <h2 style={{ color: "#7aa2e3", fontWeight: 600 }}>Quiz</h2>
+        </div>
+
+        // Button
+        <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "20px",
+            gap: "10px"
+          }}
+        >
+          <button onClick={goToHome} className="profile-button">
+            Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
