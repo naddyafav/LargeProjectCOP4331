12a1397ea1dd5816bac1916570ccd7c8170c2a3f
naddyafav <naddyafavela11@gmail.com>
2026-04-11 16:39:19 -0400

    add personalityType field to User schema

--- PersonalityApp/server/models/User.js
+++ PersonalityApp/server/models/User.js
@@ -41,7 +41,11 @@ const userSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
     }
-  ]
+  ],
+  personalityType: {
+  type: String,
+  default: null
+}
 });
 
 export default mongoose.model("User", userSchema);
