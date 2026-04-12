fa7c4999fc9bf632574746d0d82fb1559c375d22
Ben Andrew <benyaleandrew@gmail.com>
2026-04-12 15:17:26 -0400

    fixed quiz api url bug

--- PersonalityApp/client/src/pages/Quiz.tsx
+++ PersonalityApp/client/src/pages/Quiz.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API = "http://localhost:5050";
+const API = "http://104.236.41.135:5050";
 
 type Option = {
   label: string;
