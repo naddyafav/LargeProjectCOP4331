899740a6acb63c1a40d010fdf5daa933a994360d
Ben Andrew <benyaleandrew@gmail.com>
2026-04-12 22:41:14 -0400

    testing email fix

--- PersonalityApp/server/routes/regi.js
+++ PersonalityApp/server/routes/regi.js
@@ -10,11 +10,15 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: false,
-  requireTLS: true,
+  // requireTLS: true,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD
-  }
+  },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Optional: verify SMTP connection when server starts
